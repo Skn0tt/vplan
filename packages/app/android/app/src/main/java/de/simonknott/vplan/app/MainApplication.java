62dab24988b32fae0ea2cd7c523559db0d2a07e3
Simon Knott <simoknott@gmail.com>
2018-03-25 22:33:24 +0200

    Added react-native-push-notification

--- packages/app/android/app/src/main/java/de/simonknott/vplan/app/MainApplication.java
+++ packages/app/android/app/src/main/java/de/simonknott/vplan/app/MainApplication.java
@@ -3,6 +3,7 @@ package de.simonknott.vplan.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNBackgroundFetchPackage(),
             new VectorIconsPackage()
       );
