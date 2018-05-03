543792af9474bfd81ffa51601470188673eda9fd
Simon Knott <simoknott@gmail.com>
2018-05-03 06:35:09 +0000

    Add Tests for subst_011.htm

--- packages/parser/src/index.spec.ts
+++ packages/parser/src/index.spec.ts
@@ -48,6 +48,34 @@ describe("parser", () => {
     });
   }
 
+  describe("outputs the right results", () => {
+    it("subst_011.htm", async () => {
+      const file = await loadFile(__dirname + "/../res/" + "subst_011.htm");
+      const result = parseFiles([file]);
+
+      expect(result.entries.student["5A"]).toHaveLength(3);
+      expect(result.entries.student["5B"]).toHaveLength(1);
+      expect(result.entries.student["5C"]).toHaveLength(3);
+      expect(result.entries.student["5D"]).toHaveLength(3);
+      expect(result.entries.student["6A"]).toHaveLength(2);
+      expect(result.entries.student["6B"]).toHaveLength(2);
+      expect(result.entries.student["6D"]).toHaveLength(1);
+      expect(result.entries.student["7A"]).toHaveLength(1);
+      expect(result.entries.student["7B"]).toHaveLength(1);
+      expect(result.entries.student["7C"]).toHaveLength(1);
+      expect(result.entries.student["8A"]).toHaveLength(4);
+      expect(result.entries.student["8D"]).toHaveLength(1);
+      expect(result.entries.student["9A"]).toHaveLength(10);
+      expect(result.entries.student["9B"]).toHaveLength(15);
+      expect(result.entries.student["9C"]).toHaveLength(11);
+      expect(result.entries.student["9D"]).toHaveLength(10);
+      expect(result.entries.student["EF"]).toHaveLength(11);
+      expect(result.entries.student["Q1"]).toHaveLength(10);
+      expect(result.entries.student["EWS"]).toHaveLength(1);
+      expect(result.entries.student["AG"]).toHaveLength(1);
+    })
+  })
+
   describe("version detection", () => {
     it("outputs the right timestamp", async () => {
       const file = await loadFile(__dirname + "/../res/subst_001.htm");
