86568c627645a79630db9473bcc0afeaf171dff1
Simon Knott <simoknott@gmail.com>
2018-06-23 12:24:14 +0200

    change colour scheme

--- packages/app/src/routes/Home/elements/SectionHeader/styles.ts
+++ packages/app/src/routes/Home/elements/SectionHeader/styles.ts
@@ -1,16 +1,14 @@
 import { StyleSheet } from "react-native";
 import * as util from "vplan-util";
 
-const radius = 44;
-
 export default StyleSheet.create({
   container: {
     paddingLeft: 14,
     paddingVertical: 14,
-    backgroundColor: util.white + "DC"
+    backgroundColor: util.blueGray
   },
   text: {
-    color: util.lightGray,
+    color: util.white,
     fontWeight: "bold"
   }
 });
--- packages/util/src/colors.ts
+++ packages/util/src/colors.ts
@@ -9,5 +9,6 @@ export const blue900 = "#0D47A1";
 export const lightBlue500 = "#03A9F4";
 export const cyan500 = "#00BCD4";
 export const lightGray = "#616161";
+export const blueGray = "#78909c";
 export const white = "#FFFFFF";
 export const black = "#000000";
